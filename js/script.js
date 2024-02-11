ccd258535bc3db98c9e2a9a4c772b090d09cc7b1
GaziRiad <ouhibimustapha36@gmail.com>
2024-02-11 21:22:55 +0100

    made humber menu fully functional

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+  if (headerEl.classList.contains("nav-open"))
+    document.documentElement.style.overflow = "hidden";
+  else document.documentElement.style.overflow = "auto";
+
+  console.log(document.html);
   console.log("Humberger button click!");
 });
 
