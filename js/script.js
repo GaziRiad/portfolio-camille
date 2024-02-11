a4d5a4968bf0068fafd478b819b619bfd8d81216
GaziRiad <ouhibimustapha36@gmail.com>
2024-02-11 20:57:02 +0100

    finished mobile menu

---
+++ js/script.js
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+  console.log("Humberger button click!");
+});
+
+///////////////////////////////////////////////////////////
